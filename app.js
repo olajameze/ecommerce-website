2d8d51a002e075bf5ac07d5a5d35513a1e807fec
James Gomez <olagomez@live.co.uk>
2022-04-19 11:16:40 +0100

    Payment function

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const currentProductPrice = document.querySelector(".productPrice");
 
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
-//changes the slide
+ //changes the slide
       wrapper.style.transform = `translateX(${-100 * index}vw)`;
   
 //This changes the chosen product
@@ -80,3 +80,15 @@ menuItems.forEach((item, index) => {
         choosenProduct = products[index]
     });
 });
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+    productButton.addEventListener("click", () => {
+    payment.style.display = "flex";
+  });
+  
+    close.addEventListener("click", () => {
+    payment.style.display = "none";
+  })
